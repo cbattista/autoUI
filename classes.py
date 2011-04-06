d4992245d91bc69cb44afe0bc99a85ffc1625bec
christian <battista.christian@gmail.com>
2011-04-06 18:37:34 -0400

    more functionality added

--- classes.py
+++ classes.py
@@ -19,5 +19,85 @@ from wxmacros import *
 class ClassWidget(wx.GridBagSizer):
 	def __init__(self, target, parent=None, *args, **kwargs):
 		wx.GridBagSizer.__init__(self, *args, **kwargs)
-		self.target = target
 		self.parent = parent
+
+		self.target = target
+
+		self.instance = None 
+
+		self.args = inspect.getargspec(self.target.__init__)
+
+		self.argnames = self.args=[0]
+
+		self.argnames.remove('self')
+
+		self.defaults = ()
+
+		self.defaults = self.args[3]
+
+		if self.argnames:
+			self.nondefaults = len(self.argnames) - len(self.defaults)
+			while len(self.defaults) != len(self.argnames):
+				d = list(self.defaults)
+				d.insert(0, None)
+				self.defaults = tuple(d)
+		else:
+			self.nondefaults = 0
+
+
+		self.construct()
+
+	def construct(self):
+		"""create the widgets and put them in 
+		the appropriate collection"""
+
+		#list of arguments (which have their IDs searchable by .find())
+		self.args = Arguments()
+		self.methods = Methods()
+
+		#control items (buttons etc)
+		self.controls = {}
+		#all the items		
+		self.items = []
+
+
+		#init button
+		self.init = wx.Button(self.parent, -1, 'Init')
+		self.controls[str(self.init.GetId())] = self.init	
+
+		#create the arguments
+		for a, d in (self.argnames, self.defaults):
+			item = ArgWidget(a, d, self.parent)
+			self.args.append(item)
+			self.items.append(item)
+
+		self.items.append(self.init)	
+
+		#also need methods
+		for f in inspect.getmembers(self.target):
+			#if this is a user defined, public function, create a frame for its arguments
+			if inspect.isbuiltin(f[0]) or f[0].startswith('_'):
+				pass
+			else:
+				b_id += 1
+				self.functions.append(f[1])
+				button = wx.Button(self.parent, b_id, f[0])
+				button.Disable()
+				self.buttons.append(button)
+				functionFrame = objSizer(self.parent, f[1], recurse=False)
+				self.items['methods'].append(functionFrame)
+		
+	def setDefault(self, value, arg):
+		"""sets a default value for a given arg"""
+		index = self.args.index(arg)
+		defaults = list(self.defaults)
+		defaults[index] = value
+		defaults = tuple(defaults)
+		self.defaults = defaults
+
+	def layout(self):
+		"""layout the items in the sizer"""
+		layouts.LayoutGrid(self)
+
+	def onButton(self):
+		for
