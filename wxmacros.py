42d7dd46ce8970ff56d01f4b8e3fe3be939f193d
christian <battista.christian@gmail.com>
2011-04-05 22:46:29 -0400

    list widget creating and reading functions

--- wxmacros.py
+++ wxmacros.py
@@ -1,5 +1,17 @@
-#wxmacros.py
+"""
+autoUI by Christian Battista
+
+autoUI is a collection of python modules that are used to create wx apps from classes.  autogenerates a user interface which can be used to manipulate class arguments and functions.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import wx
+from values import ValWidget
 
 def ClassItems(value):
 	"""code constructor for a class/instance"""
@@ -14,7 +26,7 @@ def ClassItems(value):
 
 	items.append("wx.StaticText(self.parent, -1, str(%s)" % c)
 	items.append("wx.StaticText(self.parent, -1, str(%s)" % i)
-	items.append("wx.Button(self.parent, -1, 'Edit')")
+	items.append("wx.Button(self.parent, -1, 'Class Edit')")
 
 	return items
 
@@ -33,3 +45,21 @@ def ReadClassItems(c, i):
 		return i
 	else:
 		return c
+
+def ListItems(the_list):
+	items = []
+
+	for l in the_list:
+		item = ValWidget(l)
+		items.append(item)
+
+	items.append("wx.Button(self.parent, -1, 'List Edit')")
+
+def ReadList(items):
+	values = []
+	for i in items:
+		if hasattr(i, "read"):
+			v = i.read()
+			values.append(v)
+
+	return values
